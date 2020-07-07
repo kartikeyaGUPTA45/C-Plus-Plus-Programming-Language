00b0b65669e825613f0b7135dd2999a37ff60033
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 22:05:36 +0530

    max_size() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/max_size().cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    
+    
+    //8.max_size():This method returns the maximum size of the array.
+
+    cout<<array1.max_size()<<endl;
+
+
+    return 0;
+}

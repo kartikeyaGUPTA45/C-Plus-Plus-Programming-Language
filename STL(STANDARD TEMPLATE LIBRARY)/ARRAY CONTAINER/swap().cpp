c3deaec7b24af08368add7e9414240cb19f99640
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 21:59:30 +0530

    swap() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/swap().cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+   array<int,8> a={1,2,3,4,5,6,7,8};
+    array<int,8> b={8,7,6,5,4,3,2,1};
+    a.swap(b);
+    
+    cout << "a is : ";
+    for(int i=0; i < 8; i++) {
+    cout << a[i] <<" ";
+    }
+    cout << endl;
+    cout << "b is : ";
+    for(int i=0; i < 8; i++) {
+    cout << b[i] <<" ";
+    }
+
+    
+    return 0;
+}

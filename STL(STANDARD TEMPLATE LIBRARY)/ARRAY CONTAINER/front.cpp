c83c50c2c4e78398c32e2f13423dcad1661170ed
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 21:54:30 +0530

    front code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/front.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    // front():This method returns the first element in the array
+    cout<<array1.front()<<endl;
+
+    
+    
+    return 0;
+}

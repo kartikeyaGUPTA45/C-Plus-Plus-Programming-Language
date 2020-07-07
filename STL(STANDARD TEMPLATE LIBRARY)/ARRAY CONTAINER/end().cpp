68be38b71f9be3360b5136201c607c063c01bffa
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 22:09:27 +0530

    end() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/end().cpp
@@ -0,0 +1,16 @@
+  
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    
+    
+    //10.end():This method returns an iterator pointing to an element next to the last element in the array.
+    
+    cout<<array1.end();
+
+    return 0;
+}

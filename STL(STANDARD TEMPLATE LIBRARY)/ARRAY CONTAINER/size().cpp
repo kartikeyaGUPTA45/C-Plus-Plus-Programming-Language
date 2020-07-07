843fa00a682ad520df71e3e00e385f786c4f0370
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 22:04:23 +0530

    size() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/size().cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    
+    // size():This method returns the number of element present in the array
+    cout<<array1.size()<<endl;
+    
+    return 0;
+}

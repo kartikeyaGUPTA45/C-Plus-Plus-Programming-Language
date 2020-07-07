78abc31a9146562916d02c953c6514fc7d6e4634
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 22:01:57 +0530

    empty() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/empty().cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    
+    
+    //empty():This method can be used to check whether the array is empty or not
+    cout<<array1.empty()<<endl;	
+
+    
+    
+    return 0;
+}

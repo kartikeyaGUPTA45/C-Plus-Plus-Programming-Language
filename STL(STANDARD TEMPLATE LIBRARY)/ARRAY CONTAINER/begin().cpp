1f8e9a2855dfa6658329942e1c4696a0c9687e79
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-07 22:06:52 +0530

    begin() code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/begin().cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+
+int main()
+{
+    array<int,10> array1={1,2,3,4,5,6,7,8,9};
+    
+    
+    //begin():This method returns the iterator pointing to the first element of the array. Iterators are just like pointers
+    
+    cout<<array1.begin()<<endl;
+
+
+
+    return 0;
+}

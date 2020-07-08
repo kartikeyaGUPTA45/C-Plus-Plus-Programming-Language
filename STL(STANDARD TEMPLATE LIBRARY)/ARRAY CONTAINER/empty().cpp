c2590c6d589b58aeae1d6dc50dab455d6e303b55
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-07-08 22:38:52 +0530

    empty() code

--- STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/empty().cpp
+++ STL(STANDARD TEMPLATE LIBRARY)/ARRAY CONTAINER/empty().cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() 
 { 
     // declaration of array container 
-    array<int, 5> myarray{ 1, 2, 3, 4, 5 }; 
+    array<int, 5> array1{ 1, 2, 3, 4, 5 }; 
   
-    // using end() to print array 
-    for (auto it = myarray.begin(); 
-         it != myarray.end(); ++it) 
-        cout << ' ' << *it; 
+    //empty(): This method can be used to check whether the array is empty or not.
+    //0:represents array is not empty
+    //1:represents array is empty
+  cout<<"Is the array is:"<<array1.empty();
     return 0; 
 } 
+

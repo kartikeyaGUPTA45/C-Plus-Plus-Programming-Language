d29020d1bbcee59217021185f8ba01571faed2ec
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-08-04 16:58:33 +0530

    insert code

---
+++ LinkedList/Doubly linked list/insert in a doubly linked list.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void Create(int A[],int n)
+{
+    struct Node *t,*last;
+    int i;
+    first=new Node;
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+    
+}
+
+
+void Display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+    
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+int length(struct Node *p)
+{
+    int len=0;
+    while(p)
+    {
+        len++;
+        p=p->next;
+    }
+    return len;
+}
+
+void Insert(struct Node *p,int index,int x)
+{
+    struct Node *t;
+    int i;
+    if(index<0 ||index>length(p))
+        return;
+    if(index==0)
+    {
+        t=new Node;
+        t->data=x;
+        t->prev=NULL;
+        t->next=first;
+        first->prev=t;
+        first=t;
+        
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)
+            p=p->next;
+        t=new Node;
+        t->data=x;
+        t->prev=p;
+        t->next=p->next;
+        if(p->next)p->next->prev=t;
+        p->next=t;
+            
+    }
+}
+int main()
+{
+    int A[]={10,20,30,40,50,60};
+    Create(A,6);
+    Display(first);
+    cout<<"\nLength of Doubly Linked List is:"<<length(first);
+    Insert(first,3,35);
+    cout<<"\n";
+    Display(first);
+    return 0;
+}

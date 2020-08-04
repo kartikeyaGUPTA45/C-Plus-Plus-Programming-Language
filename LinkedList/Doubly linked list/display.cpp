ded77ab9646f7e41b5be60e12cfd264919bf0ea4
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-08-04 15:53:53 +0530

    display

---
+++ LinkedList/Doubly linked list/display.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void Create(int A[],int n)
+{
+    struct Node *t,*last;
+    int i;
+    first=new Node;
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+    
+}
+
+
+void Display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+    
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+int main()
+{
+    int A[]={10,20,30,40,50,60};
+    Create(A,6);
+    Display(first);
+    return 0;
+}

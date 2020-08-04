bb14c197f0db3c67318ee506bd2b6377ff76ca96
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-08-04 17:05:34 +0530

    delete code

---
+++ LinkedList/Doubly linked list/delete in a doubly linked list.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void Create(int A[],int n)
+{
+    struct Node *t,*last;
+    int i;
+    first=new Node;
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++)
+    {
+        t=new Node;
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+    
+}
+
+
+void Display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+    
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+int length(struct Node *p)
+{
+    int len=0;
+    while(p)
+    {
+        len++;
+        p=p->next;
+    }
+    return len;
+}
+
+int Delete(struct Node*p,int index)
+{
+    int x=-1;
+    int i;
+    if(index<1 ||index>length(p))
+        return -1;
+    if(index==1)
+    {
+        first=first->next;
+        if(first)
+            first->prev=NULL;
+        x=p->data;
+        free(p);
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)
+            p=p->next;
+        p->prev->next=p->next;
+        if(p->next)
+            p->next->prev=p->prev;
+        x=p->data;
+        free(p);
+    }
+    return x;
+}
+int main()
+{
+    int A[]={10,20,30,40,50,60};
+    Create(A,6);
+    Display(first);
+    cout<<"\nLength of Doubly Linked List is:"<<length(first);
+    Delete(first,3);
+    cout<<"\n";
+    Display(first);
+    return 0;
+}

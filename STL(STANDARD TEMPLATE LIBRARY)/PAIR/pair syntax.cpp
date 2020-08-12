a464a7ff7bef8eec079ab37bc63a7b739750049c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-08-12 19:44:17 +0530

    pair syntax

---
+++ STL(STANDARD TEMPLATE LIBRARY)/PAIR/pair syntax.cpp
@@ -0,0 +1,14 @@
+ #include<iostream>
+ using namespace std;
+int main() 
+{ 
+	pair <int, char> PAIR1 ; //Syntax
+
+	PAIR1.first = 100; 
+	PAIR1.second = 'G' ; 
+
+	cout << PAIR1.first << " " ; 
+	cout << PAIR1.second << endl ; 
+
+	return 0; 
+} 

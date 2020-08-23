66d45870daa821b759a885e5086030141fbb7316
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-08-23 19:15:37 +0530

    height of heap

---
+++ HEAP/Height of Heap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+	  int n;
+	  cin>>n;
+	 int a[n];
+	 for(int i=0;i<n;i++)
+       cin>>a[i];
+	int x=log2(n);
+	cout<<x<<endl;
+	return 0;
+}

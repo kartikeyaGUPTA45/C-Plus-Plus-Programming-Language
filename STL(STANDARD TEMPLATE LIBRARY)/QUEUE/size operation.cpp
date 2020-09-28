7598c8d95b4820942c0856bc5571112e2dbcb56a
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-09-28 16:44:33 +0530

    size code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/QUEUE/size operation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    cout<<"Queue Size:-"<<q.size()<<endl;
+    return 0;
+}

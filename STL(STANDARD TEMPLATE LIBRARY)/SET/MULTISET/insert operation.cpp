934138e34fb8be61fe20a8ddecc5d64895ac4c3f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-10-02 17:25:10 +0530

    insert code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/SET/MULTISET/insert operation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main()
+{
+    multiset<int>s;
+    s.insert(55);
+    s.insert(10);
+    s.insert(45);
+    s.insert(45);
+    cout<<"Elements of Multiset are:";
+    for(auto i:s)
+        cout<<i<<" ";
+    return 0;
+}

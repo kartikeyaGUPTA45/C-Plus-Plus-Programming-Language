74c57111a553c9c8d76c86ec23c3546afc3c1528
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2020-10-02 16:56:18 +0530

    upper_bound code

---
+++ STL(STANDARD TEMPLATE LIBRARY)/SET/upper_bound operation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main()
+{
+    set<int>s;
+    s.insert(10);
+    s.insert(5);
+    s.insert(20);
+    s.insert(27);
+    auto it=s.upper_bound(10);
+    if(it!=s.end())
+        cout<<"Element is:"<<(*it)<<" ";
+    else
+        cout<<"Given element is greater than the largest";
+    return 0;
+}
